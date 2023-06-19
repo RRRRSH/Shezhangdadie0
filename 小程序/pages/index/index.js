a6353ab59db4c9a68928b254b8e27f809189a902
RRRRSH <137074317+RRRRSH@users.noreply.github.com>
2023-06-19 21:52:28 +0800

    Update index.js

--- 小程序/pages/index/index.js
+++ 小程序/pages/index/index.js
@@ -1,1 +1,21 @@
+const app = getApp()
 
+Page({
+  data: {
+    switchValue: "none",
+    radioValue: "none",
+    checkBoxValue: "none",
+    timePickerValue: "09:30",
+    sliderValue: 0
+  },
+  switchChange: function (e) {
+    this.setData({
+      switchValue: e.detail.value
+    }),
+      console.log('switch发生change事件，携带value值为：', e.detail.value)
+  },
+ 
+
+  onLoad: function (options) {
+  },
+})
